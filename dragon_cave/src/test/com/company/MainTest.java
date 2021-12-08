c89cf063bede4b1572b4b3c08fd79cc55c0b4973
Chanzik Brooks <chanzik01@gmail.com>
2021-12-08 15:35:08 -0500

    Added JUnit test class

---
+++ dragon_cave/src/test/com/company/MainTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    Main main;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        main = new Main();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
