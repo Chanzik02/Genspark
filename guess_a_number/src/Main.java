9c4e3158c0722b5e53c0c8f2575f51e8559830aa
Chanzik Brooks <chanzik01@gmail.com>
2021-12-08 14:16:34 -0500

    Added Exception and Changes
    Fixed infinite loops

--- guess_a_number/src/Main.java
+++ guess_a_number/src/Main.java
@@ -3,83 +3,120 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
+        String name = "";
         int aRandomNumber = random.nextInt(21);
         int guesses = 1;
-
         String playOrNot = "";
-        Boolean isRunning = true;
+        Boolean hasLettersOnly = true;
+        //Boolean isRunning = true;
 
-        System.out.println("Hello! What is your name?");
+        try {
 
+            System.out.println("Hello! What is your name?");
 
-        String name = sc.nextLine();
+            name = sc.nextLine();
 
-        do{
+            hasLettersOnly = name.matches("[a-zA-Z]+");
+            System.out.println(hasLettersOnly);
 
-            System.out.println("Well " + name + " I am thinking of a number between 1 and 20.");
-            System.out.println("Take a guess.");
+            if(!hasLettersOnly) {
+                while (!hasLettersOnly) {
+                    System.out.println("Your name should only include letters.");
 
-            String choice = sc.nextLine();
-            int choiceAsInt = Integer.parseInt(choice);
+                    System.out.println("Hello! What is your name?");
 
-            System.out.println(aRandomNumber);
+                    name = sc.nextLine();
 
-
-            if(choiceAsInt == aRandomNumber){
-                guesses++;
-                System.out.println("Good job " + name + " You guessed my number in " + guesses + " guesses!");
-
-            }
-            else if(choiceAsInt != aRandomNumber) {
-                while(guesses != 3) {
-                    if(choiceAsInt > aRandomNumber) {
-                        System.out.println("Your guess is too high");
-                        guesses++;
-                        System.out.println("Try again.");
-                        choice = sc.nextLine();
-                        choiceAsInt = Integer.parseInt(choice);
-                    }
-                    else if(choiceAsInt < aRandomNumber) {
-                        System.out.println("Your guess is too low.");
-                        guesses++;
-                        System.out.println("Try again.");
-                        choice = sc.nextLine();
-                        choiceAsInt = Integer.parseInt(choice);
-
-                    }
+                    hasLettersOnly = name.matches("[a-zA-Z]+");
                 }
 
             }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
 
-            System.out.println("Would you like to play again? (y/n)");
-            playOrNot = sc.nextLine();
-            if(playOrNot.equals("y")) {
-                guesses = 0;
-            }
-            else if(playOrNot.equals("n")) {
-                guesses = 3;
+        try {
 
-            }
-            else {
-                System.out.println("Please pick y/n");
+            do{
+
+                System.out.println("Well " + name + " I am thinking of a number between 1 and 20.");
+                System.out.println("Take a guess.");
+                System.out.println(aRandomNumber);
+
+                String choice = sc.nextLine();
+                int choiceAsInt = Integer.parseInt(choice);
+
+
+
+                if(choiceAsInt == aRandomNumber){
+                    guesses++;
+                    System.out.println("Good job " + name + " You guessed my number in " + guesses + " guesses!");
+
+                }
+                else if(choiceAsInt != aRandomNumber) {
+                    while(guesses != 6 || choiceAsInt == aRandomNumber) {
+                        if(choiceAsInt == aRandomNumber) {
+                            //guesses++;
+                            System.out.println("Good job " + name + " You guessed my number in " + guesses + " guesses!");
+                            break;
+                        }
+                        else if(choiceAsInt > aRandomNumber) {
+                            System.out.println("Your guess is too high");
+                            guesses++;
+                            System.out.println("Try again.");
+                            choice = sc.nextLine();
+                            choiceAsInt = Integer.parseInt(choice);
+                        }
+                        else if(choiceAsInt < aRandomNumber) {
+                            System.out.println("Your guess is too low.");
+                            guesses++;
+                            System.out.println("Try again.");
+                            choice = sc.nextLine();
+                            choiceAsInt = Integer.parseInt(choice);
+
+                        }
+
+                    }
+
+                }
+
+
                 System.out.println("Would you like to play again? (y/n)");
                 playOrNot = sc.nextLine();
                 if(playOrNot.equals("y")) {
                     guesses = 1;
-                    aRandomNumber = random.nextInt(21);
-
                 }
                 else if(playOrNot.equals("n")) {
-                    guesses = 3;
+                    guesses = 6;
 
                 }
-            }
+                else {
+                    System.out.println("Please pick y/n");
+                    System.out.println("Would you like to play again? (y/n)");
+                    playOrNot = sc.nextLine();
+                    if(playOrNot.equals("y")) {
+                        guesses = 1;
+                        aRandomNumber = random.nextInt(21);
 
-        } while(guesses !=3);
+                    }
+                    else if(playOrNot.equals("n")) {
+                        guesses = 6;
+                        //break;
+                    }
+                }
+
+            } while(guesses !=6);
+
+        } catch (Exception e) {
+            //e.printStackTrace();
+            throw new Exception("This is not a number.");
+        }
+
+
 
 
 
